c2ff57860746dc9b26515dc72a5243b944b14c7e
root <root@1af5.k.hostens.cloud>
2018-04-15 20:04:39 +0300

    first commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+	const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x9c, 0x50, 0x4d, 0x08, 0x7f, 0x92, 0x5c, 0x2d, 0x62, 0x3f, 0x7c, 0x2a, 0x34, 0x35, 0x6d, 0x92 } };
+  //const static boost::uuids::uuid BYTECOIN_NETWORK = { { 0x11 ,0x10, 0x01, 0x11 , 0x11, 0x00 , 0x01, 0x01, 0x10, 0x11, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10} }; //Bender's nightmare
+}
